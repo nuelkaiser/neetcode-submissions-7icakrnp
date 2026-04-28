7c29cb2d198e15ad514ab828cedf8e5effda4647
Immanuel <majekodunmiimmanuel9@gmail.com>
2026-04-28 19:32:23 +0100

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
+        dfs(nums, new ArrayList<>(), new boolean[nums.length]);
+        
+        return res;
+    }
+    
+    public void dfs(int[] nums, List<Integer> curr, boolean[] pick) {
+        if(curr.size() == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!pick[i]) {
+                curr.add(nums[i]);
+                pick[i] = true;
+                dfs(nums, curr, pick);
+                curr.remove(curr.size() - 1);
+                pick[i] = false;
+            }
+        }
+    }
+}
